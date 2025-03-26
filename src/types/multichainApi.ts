7d9f442a137095fc3926d700207c7e62bac3ea51
Baptiste Marchand <75846779+baptiste-marchand@users.noreply.github.com>
2025-03-26 16:42:48 +0100

    Replace interfaces by types

--- src/types/multichainApi.ts
+++ src/types/multichainApi.ts
@@ -5,13 +5,13 @@ export type MultichainApiMethod = keyof MultichainApi<any>;
 export type MultichainApiParams<T extends RpcApi, M extends MultichainApiMethod> = Parameters<MultichainApi<T>[M]>[0];
 export type MultichainApiReturn<T extends RpcApi, M extends MultichainApiMethod> = ReturnType<MultichainApi<T>[M]>;
 
-// Interface for transport layer mapping to the multichain api methods
-export interface MultichainApiClient<T extends RpcApi = DefaultRpcApi> {
+// Type for transport layer mapping to the multichain api methods
+export type MultichainApiClient<T extends RpcApi = DefaultRpcApi> = {
   createSession: MultichainApi<T>['wallet_createSession'];
   getSession: MultichainApi<T>['wallet_getSession'];
   revokeSession: MultichainApi<T>['wallet_revokeSession'];
   invokeMethod: MultichainApi<T>['wallet_invokeMethod'];
-}
+};
 
 // Multichain API Methods
 export type MultichainApi<T extends RpcApi> = {
@@ -24,10 +24,10 @@ export type MultichainApi<T extends RpcApi> = {
 };
 
 // wallet_createSession params
-export interface CreateSessionParams<T extends RpcApi> {
+export type CreateSessionParams<T extends RpcApi> = {
   requiredScopes?: Record<Scope<T>, ScopeObject>;
   optionalScopes?: Record<Scope<T>, ScopeObject>;
-}
+};
 
 // wallet_invokeMethod params
 export type InvokeMethodParams<T extends RpcApi, S extends Scope<T>, M extends MethodName<T, S>> = {
